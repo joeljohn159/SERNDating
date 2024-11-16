bd32856642b97f478a8b58f993daf502043fff2b
amrit <amritcsadhikari@gmail.com>
2024-11-15 18:00:55 -0600

    add create,update and get profiles apis

---
+++ server/router/profileRouter.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import jwtVerify from '../middleware/jwtVerify.js';
+import profileController from '../controller/ProfileController.js';
+import expressValidator from '../middleware/expressValidator.js';
+
+const profileRouter = express.Router();
+
+/**
+ * @usage: Create a Profile
+ * @url: http://localhost:8080/profiles/
+ * @params: age, gender, location, bio
+ * @method: POST
+ * @access: PRIVATE
+ */
+profileRouter.post(
+    '/',
+    jwtVerify,
+    expressValidator,
+    profileController.createProfile
+);
+
+/**
+ * @usage: Get User's Profile
+ * @url: http://localhost:8080/profiles/me/
+ * @params: none
+ * @method: GET
+ * @access: PRIVATE
+ */
+profileRouter.get('/me', jwtVerify, profileController.getProfile);
+
+/**
+ * @usage: Update Profile
+ * @url: http://localhost:8080/profiles/me/
+ * @params: age, gender, location, bio
+ * @method: PUT
+ * @access: PRIVATE
+ */
+profileRouter.put('/me', jwtVerify, expressValidator, profileController.updateProfile);
+
+export default profileRouter;
